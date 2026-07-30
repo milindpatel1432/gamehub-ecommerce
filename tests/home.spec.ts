e7412f9de08d9d8edc7a6c647fe8eba9c50a1663
milindpatel1432 <milindpatel1432@gmail.com>
2026-07-30 10:49:00 +0530

    refactor: completely remove Playwright E2E testing setup

--- tests/home.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Home page should load successfully', async ({ page }) => {
-  await page.goto('/');
-  await expect(page).toHaveTitle(/GameHub/i);
-});
